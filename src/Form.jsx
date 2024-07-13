ef908cff7ec6d81c64d725aa5ab287c068cbe327
Buntcz <testtigeryt@gmail.com>
2024-07-13 20:16:48 +0300

    found a way to connect the form with the inputs, but there is some kind of an issue that I need to fix.

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,16 +1,31 @@
+import { useState } from "react"
+
 function Form({cl,condition}) {
+   const[title,setTitle] =  useState("")
+   const[ingredients,setIngredients] = useState("")
+   const[wayToMake,setWayToMake] = useState("")
+
+   function handleSubmit(event) {
+     event.prevent.default()
+     console.log({
+        'title': title,
+        'ingredients': ingredients,
+        'Way to make': wayToMake
+     })
+   }
+
     return (
-        <form className={condition ? cl + " active": cl} noValidate>
+        <form className={condition ? cl + " active": cl} onSubmit={() => handleSubmit()}>
             <h1>Add Recipe</h1>
             <p>please fill up the fields under with the appropriate information.</p>
             <label className="title">
-               Title: <input type="text" id="title" />
+               Title: <input type="text" id="title" value={title}  onChange={(e) => setTitle(e.target.value)} />
             </label>
             <label className="ingredients">
-                Ingredients: <input type="text" id="ingredients"/>
+                Ingredients: <input type="text" id="ingredients" value={ingredients}  onChange={(e) => setIngredients(e.target.value)}/>
             </label>
             <label className="wayToMake">
-                Way to make : <input type="text" id="wayToMake" />
+                Way to make : <input type="text" id="wayToMake" value={wayToMake}  onChange={(e) => setWayToMake(e.target.value)} />
             </label>
             <button type="submit">Submit Recipe</button>
         </form>
